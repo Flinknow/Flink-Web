1fc5fd4a97c976d75024ec7116bd5d63b0c1c1b8
rishakrsharma <sharmarishav.srk@gmail.com>
2022-02-25 05:10:24 +0530

    Hero section and footer done

--- components/Footer.js
+++ components/Footer.js
@@ -1,29 +1,9 @@
-/* This example requires Tailwind CSS v2.0+ */
-const navigation = {
-  solutions: [
-    { name: "Marketing", href: "#" },
-    { name: "Analytics", href: "#" },
-    { name: "Commerce", href: "#" },
-    { name: "Insights", href: "#" },
-  ],
-  support: [
-    { name: "Pricing", href: "#" },
-    { name: "Documentation", href: "#" },
-    { name: "Guides", href: "#" },
-    { name: "API Status", href: "#" },
-  ],
+const navigation = { 
   company: [
-    { name: "About", href: "#" },
-    { name: "Blog", href: "#" },
-    { name: "Jobs", href: "#" },
-    { name: "Press", href: "#" },
-    { name: "Partners", href: "#" },
-  ],
-  legal: [
     { name: "Claim", href: "#" },
     { name: "Privacy", href: "#" },
     { name: "Terms", href: "#" },
-  ],
+  ],  
   social: [
     {
       name: "Facebook",
@@ -97,90 +77,53 @@ export default function Footer() {
       </h2>
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
-          <div className="space-y-8 xl:col-span-1">
-            <img
-              className="h-10"
-              src="https://tailwindui.com/img/logos/workflow-mark-gray-300.svg"
-              alt="Company name"
-            />
-            <p className="text-gray-500 text-base">
-              Making the world a better place through constructing elegant
-              hierarchies.
+          <div className="space-y-5 xl:col-span-1">
+            <img className="h-32" src="/footer-logo.png" alt="Company name" />
+            <p className="text-gray-500 text-lg text-base">
+              Enhancing your styling experiences
             </p>
-            <div className="flex space-x-6">
-              {navigation.social.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className="text-gray-400 hover:text-gray-500"
-                >
-                  <span className="sr-only">{item.name}</span>
-                  <item.icon className="h-6 w-6" aria-hidden="true" />
-                </a>
-              ))}
-            </div>
           </div>
-          <div className="mt-12 grid grid-cols-2 gap-8 xl:mt-0 xl:col-span-2">
-            <div className="md:grid md:grid-cols-2 md:gap-8">
+          <div className="mt-12 grid grid-cols-1 gap-1 xl:mt-0 xl:col-span-2">
+            <div className="md:grid md:grid-cols-3 md:gap-8">
               <div>
-                <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                  Solutions
+                <h3 className="text-sm font-semibold text-gray-700 tracking-wider uppercase">
+                  Follow Us
                 </h3>
-                <ul role="list" className="mt-4 space-y-4">
-                  {navigation.solutions.map((item) => (
-                    <li key={item.name}>
-                      <a
-                        href={item.href}
-                        className="text-base text-gray-500 hover:text-gray-900"
-                      >
-                        {item.name}
-                      </a>
-                    </li>
+                <div className="flex space-x-4 pt-5">
+                  {navigation.social.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="text-indigo-500 hover:text-indigo-800"
+                    >
+                      <span className="sr-only">{item.name}</span>
+                      <item.icon className="h-6 w-6" aria-hidden="true" />
+                    </a>
                   ))}
-                </ul>
+                </div>
               </div>
               <div className="mt-12 md:mt-0">
-                <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                  Support
+                <h3 className="text-sm font-semibold text-gray-700 tracking-wider uppercase">
+                  Contact Us
                 </h3>
-                <ul role="list" className="mt-4 space-y-4">
-                  {navigation.support.map((item) => (
-                    <li key={item.name}>
-                      <a
-                        href={item.href}
-                        className="text-base text-gray-500 hover:text-gray-900"
-                      >
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
+                <div className="mt-4 space-y-1">
+                  <p className="text-base text-gray-600 ">
+                    flinkapp@gmail.com
+                  </p>
+                  <p className="text-base text-gray-600 ">
+                  +91 1234567890
+                  </p>
+                  <p className="text-base text-gray-600">
+                  Bangalore, India
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="md:grid md:grid-cols-2 md:gap-8">
               <div>
-                <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                  Company
+                <h3 className="text-sm font-semibold text-gray-700 tracking-wider uppercase">
+                  Terms & Conditions
                 </h3>
-                <ul role="list" className="mt-4 space-y-4">
+                <ul role="list" className="mt-4 space-y-1">
                   {navigation.company.map((item) => (
-                    <li key={item.name}>
-                      <a
-                        href={item.href}
-                        className="text-base text-gray-500 hover:text-gray-900"
-                      >
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="mt-12 md:mt-0">
-                <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                  Legal
-                </h3>
-                <ul role="list" className="mt-4 space-y-4">
-                  {navigation.legal.map((item) => (
                     <li key={item.name}>
                       <a
                         href={item.href}
@@ -197,7 +140,7 @@ export default function Footer() {
         </div>
         <div className="mt-12 border-t border-gray-200 pt-8">
           <p className="text-base text-gray-400 xl:text-center">
-            &copy; 2020 Workflow, Inc. All rights reserved.
+            &copy; 2020 Flink. All rights reserved.
           </p>
         </div>
       </div>
--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import Image from "next/image";
 
 const navigation = [
   { name: "Product", href: "#" },
@@ -58,55 +59,49 @@ export default function Example() {
         </svg>
       </div>
 
-      <div className="relative pt-6 pb-16 sm:pb-24 lg:pb-32">
-        <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
+      <div className="relative pt-1 pb-16 sm:pb-24 lg:pb-32">
+        <main className="mt-16 mx-auto max-w-8xl px-10 ">
           <div className="lg:grid lg:grid-cols-12 lg:gap-8">
-            <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+            <div className="sm:mt-20 sm:px-0 lg:mt-28 sm:text-center md:max-w-5xl md:mx-auto lg:col-span-7 lg:text-left">
               <h1>
-                <span className="mt-1 block text-4xl tracking-tight font-extrabold sm:text-5xl xl:text-6xl">
-                  <span className="block text-gray-900">
-                    Data to enrich your
+                <span className="mt-1 block text-5xl tracking-tight font-extrabold sm:text-6xl xl:text-7xl">
+                  <span className="font-sans block text-indigo-600">
+                    Salon Bookings
                   </span>
-                  <span className="block text-indigo-600">online business</span>
+                  <span className="block text-gray-500">
+                    At your fingertips.
+                  </span>
+                  
                 </span>
               </h1>
-              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat aliqua ad ad non deserunt sunt.
+              <p className="mt-5 text-base text-gray-500 sm:mt-7 sm:text-xl lg:text-lg xl:text-xl">
+                Get the Flink App today and book your Salon appointment in Blink
               </p>
-              <div className="mt-8 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
-                <p className="text-base font-medium text-gray-900">
-                  Sign up to get notified when it’s ready.
-                </p>
-                <form action="#" method="POST" className="mt-3 sm:flex">
-                  <label htmlFor="email" className="sr-only">
-                    Email
-                  </label>
-                  <input
-                    type="text"
-                    name="email"
-                    id="email"
-                    className="block w-full py-3 text-base rounded-md placeholder-gray-500 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:flex-1 border-gray-300"
-                    placeholder="Enter your email"
-                  />
-                  <button
-                    type="submit"
-                    className="mt-3 w-full px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto"
-                  >
-                    Notify me
-                  </button>
-                </form>
-                <p className="mt-3 text-sm text-gray-500">
-                  We care about the protection of your data. Read our
-                  <a href="#" className="font-medium text-gray-900 underline">
-                    Privacy Policy
-                  </a>
-                  .
-                </p>
-              </div>
+              <div className="mt-8 w-full sm:mx-auto sm:max-w-lg lg:ml-0">
+                    <div className="flex flex-wrap items-start justify-start">
+                      <div className="flex justify-center px-1">
+                        <a href="https://play.google.com/store" target="_blank">
+                        <img
+                          className="h-12 sm:h-14"
+                          src="/playstore.png"
+                          alt="Google PlayStore"
+                        />
+                        </a>
+                      </div>
+                      
+                      <div className="flex justify-center px-1">
+                      <a href="https://www.apple.com/in/app-store/" target="_blank">
+                        <img
+                          className="h-12 sm:h-14"
+                          src="/appstore.png"
+                          alt="Apple App Store"
+                        />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
             </div>
-            <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+            <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-5 lg:flex lg:items-center">
               <svg
                 className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-8 scale-75 origin-top sm:scale-100 lg:hidden"
                 width={640}
@@ -148,37 +143,8 @@ export default function Example() {
                   fill="url(#4f4f415c-a0e9-44c2-9601-6ded5a34a13e)"
                 />
               </svg>
-              <div className="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
-                <button
-                  type="button"
-                  className="relative block w-full bg-white rounded-lg overflow-hidden focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  <span className="sr-only">Watch our video to learn more</span>
-                  <img
-                    className="w-full"
-                    src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-                    alt=""
-                  />
-                  <div
-                    className="absolute inset-0 w-full h-full flex items-center justify-center"
-                    aria-hidden="true"
-                  >
-                    <svg
-                      className="h-20 w-20 text-indigo-500"
-                      fill="currentColor"
-                      viewBox="0 0 84 84"
-                    >
-                      <circle
-                        opacity="0.9"
-                        cx={42}
-                        cy={42}
-                        r={42}
-                        fill="white"
-                      />
-                      <path d="M55.5039 40.3359L37.1094 28.0729C35.7803 27.1869 34 28.1396 34 29.737V54.263C34 55.8604 35.7803 56.8131 37.1094 55.9271L55.5038 43.6641C56.6913 42.8725 56.6913 41.1275 55.5039 40.3359Z" />
-                    </svg>
-                  </div>
-                </button>
+              <div className="relative mx-auto w-full rounded-lg  lg:max-w-md">
+                <img className="w-full" src="/hero.png" alt="" />
               </div>
             </div>
           </div>
